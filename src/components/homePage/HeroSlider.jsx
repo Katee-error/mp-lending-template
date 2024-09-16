30060f9cbae02da68aa9ca9022c15b73e0731e9f
MalvinaCrunchy <102178640+MalvinaCrunchy@users.noreply.github.com>
2024-09-16 21:07:44 +0300

    Add files via upload

--- src/components/homePage/HeroSlider.jsx
+++ src/components/homePage/HeroSlider.jsx
@@ -1,18 +1,26 @@
-import {useEffect, useRef, useState} from "react";
-import {Box, Button, Flex, Image, Skeleton, Text,} from "@chakra-ui/react";
+import React, { useState, useEffect, useRef } from "react";
+import {
+    Box,
+    Image,
+    Text,
+    Flex,
+    Button,
+    Skeleton,
+    HStack,
+} from "@chakra-ui/react";
 import Slider from "react-slick";
-import {motion} from "framer-motion";
-
+import { motion } from "framer-motion";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import items from "../../data/items.js";
 
 import slide01 from "../../assets/slider/slide-1.png";
 import slide02 from "../../assets/slider/slide-2.png";
 import slide03 from "../../assets/slider/slide-3.png";
 
-const HeroSlider = () => {
+const SliderTest = () => {
     const settings = {
-        dots: false,
+        dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 1,
@@ -21,115 +29,116 @@ const HeroSlider = () => {
         autoplaySpeed: 3500,
         fade: false,
         cssEase: "ease",
-        pauseOnHover: false,
+        pauseOnHover: true,
     };
 
     const sliderRef = useRef(null);
-    const images = [
-        slide01,
-        slide02,
-        slide03
-    ];
-    const MotionButton = motion(Button);
-    const [isModalOpen, setIsModalOpen] = useState(false); //modal window
+    // const items = [
+    //     {
+    //         img: slide01,
+    //         title: "Товар 1",
+    //         description:
+    //             "ANNEMORE упорно следует своей уникальной философии - создавать вещи для всех аспектов повседневной жизни. Будь то батон или рубашка, каждый продукт разрабатывается с особым вниманием к деталям, чтобы обеспечить высокое качество и надежность.",
+    //     },
+    //     {
+    //         img: slide02,
+    //         title: "Товар 2",
+    //         description:
+    //             "ANNEMORE упорно следует своей уникальной философии - создавать вещи для всех аспектов повседневной жизни. Будь то батон или рубашка, каждый продукт разрабатывается с особым вниманием к деталям, чтобы обеспечить высокое качество и надежность.",
+    //     },
+    //     {
+    //         img: slide03,
+    //         title: "Товар 3",
+    //         description:
+    //             "ANNEMORE упорно следует своей уникальной философии - создавать вещи для всех аспектов повседневной жизни. Будь то батон или рубашка, каждый продукт разрабатывается с особым вниманием к деталям, чтобы обеспечить высокое качество и надежность.",
+    //     },
+    // ];
 
     const [imagesLoaded, setImagesLoaded] = useState(false);
 
     useEffect(() => {
         const loadImages = () => {
             let loaded = 0;
-            images.forEach((image) => {
+            items.forEach((item) => {
                 const img = new window.Image();
-                img.src = image;
+                img.src = item.img;
                 img.onload = () => {
                     loaded++;
-                    if (loaded === images.length) {
+                    if (loaded === items.length) {
                         setImagesLoaded(true);
                     }
                 };
             });
         };
         loadImages();
-    }, [images]);
+    }, []);
 
     return imagesLoaded ? (
-        <Box
-            id="hero"
-            width={{base: '100vw'}}
-            height={{base: "50vh", lg: '600px'}}
-            // minH={'600px'}
-            overflow="hidden"
-            position="relative"
-            textAlign="center"
-        >
+        <Box width="100%" mx="auto" height="800px" position="relative">
             <Slider {...settings} ref={sliderRef}>
-                {images.map((image, index) => (
-                    <Flex
-                        flexDirection={{base: 'column', sm: 'row'}}
-                        key={index} position="relative" bgColor={'#e8e8e8'} h={'600px'}>
-                        <Box
-                            position={'absolute'}
-                            top={{base: '8%', xxxs: '7%', xxs: '7%', '510px': '10%', xs: '10%', s: '80%',sm: '50%'}}
-                            left={{base: '20%', xxxs: '20%', xxs: '20%', xs: '80%', s: '80%',sm: '40%'}}
-                            // h={{base: '60%', xxxs: '60%', xxs: '60%', xs: '80%', s: '80%', sm:'80%', md: '80%' }}
-                            // w={{base: '60%', xxxs: '100%', xxs: '100%', xs: '20%', s: '20%', sm:'20%', md: '20%' }}
-                            w={{base:'70%', xxxs: '65%', xxs:'55%', sm:'37%'}}
-                        >
-                            <Image
-                                ml={'auto'}
-                                src={image}
-                                alt={`Slide ${index + 1}`}
-                                // style={{width: '37%'}}
-                            />
-                        </Box>
-                        <Box
-                            position="absolute"
-                            top={{base: '75%', sm: '50%'}}
-                            left={{base: '50%', sm: '40%'}}
-                            transform="translate(-50%, -50%)"
+                {items.map((item, index) => (
+                    <Box
+                        key={index}
+                        flexDirection={{ base: "column", sm: "row-reverse" }}
+                        display="flex !important"
+                        alignItems="center"
+                        justifyContent="space-around"
+                        bgColor={"#e8e8e8"}
+                        height="700px"
+                        borderRadius="10px"
+                        p="40px"
+                        mx="auto"
+                    >
+                        {/* Изображение */}
+                        <Image
+                            src={item.img}
+                            alt={`Image ${index + 1}`}
+                            h={"100%"}
+                            w={{base: '100%',sm:"50%"}}
+                            objectFit="contain"
+                        />
+
+                        {/* Блок с текстом */}
+                        <Flex
+                            flexDirection="column"
+                            justifyContent="center"
                             textAlign="start"
-                            zIndex={1000}
-                            p="20px"
-                            width={{base: "100%", md: "70%"}}
-                            color={'#515151'}
+                            width={{ base: "100%", md: "40%" }}
+                            gap={'20px'}
                         >
-                            <Text fontWeight={500} fontSize={{base: "30px", md: '66px'}}>
-                                Покупай заебал
+                            <Text fontWeight={500} fontSize={{ base: "30px", md: "66px" }}>
+                                {item.title}
                             </Text>
                             <Text
                                 fontWeight={500}
-                                fontSize={{base: "12px", md: '18px'}}
-                                maxW={'650px'}
+                                fontSize={{ base: "12px", md: "18px" }}
                                 mt="-10px"
                             >
-                                ANNEMORE упорно следует своей уникальной философии - создавать вещи для всех аспектов
-                                повседневной жизни. Будь то батон или рубашка, каждый продукт разрабатывается с особым
-                                вниманием к деталям, чтобы обеспечить высокое качество и надежность.
+                                {item.description}
                             </Text>
                             <Button
-                                p={{base: "10px 20px", md: "25px 30px"}}
+                                p={{ base: "10px 20px", md: "25px 30px" }}
                                 borderRadius="10px"
                                 fontSize="16px"
                                 fontWeight={600}
                                 bg="#c98b73"
-                                _hover={{bgColor: "#ae7963"}}
-                                whileHover={{scale: 1.05}}
+                                _hover={{ bgColor: "#ae7963" }}
+                                whileHover={{ scale: 1.05 }}
                                 color="white"
                                 mt="20px"
-                            >Купить
-
+                            >
+                                Купить
                             </Button>
-
-                        </Box>
-                    </Flex>
+                        </Flex>
+                    </Box>
                 ))}
             </Slider>
         </Box>
     ) : (
         <Box>
-            <Skeleton h={{base: '300px', md: '700px'}} w={'100%'}></Skeleton>
+            <Skeleton h={{ base: "300px", md: "700px" }} w="100%" />
         </Box>
     );
 };
 
-export default HeroSlider;
+export default SliderTest;
--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,106 +1,176 @@
-import {
-    Box,
-    Container,
-    Flex,
-    Image,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalFooter,
-    Text
-} from "@chakra-ui/react";
-import Catalog from "../components/items/Catalog.jsx";
-import HeroSlider from "../components/homePage/HeroSlider.jsx";
+import {Box, Button, Container, Flex, HStack, Image, Text} from "@chakra-ui/react";
+import ItemImg from '../assets/items/item-1.jpg'
 import StarFill from "../assets/icons/star-fill.svg";
 import StarOutline from "../assets/icons/star-outline.svg";
-import formatTextWithLineBreaks from "../services/split.jsx";
-import items from "../data/items.js";
-import ItemsCard from "../components/items/ItemsCard.jsx";
+import React from "react";
+import {motion} from "framer-motion";
+import './style.css'
 
-
-
+const MotionButton = motion(Button);
 
 const CatalogPage = (item) => {
-  return (
+    return (
 
-    <Box>
-      <Container maxW="container.xl">
-      {items.map((item, index) => (
-              <Flex
-                  gap={{ base: "20px", md: "40px" }}
-                  alignItems={"center"}
-                  flexDirection={{ base: "column", md: "row" }}
-              >
-                  <Image
-                      src={item.img}
-                      w={"auto"}
-                      h={"600px"}
-                      objectFit={"contain"}
-                  />
-                  <Box>
-                      <Flex
-                          justifyContent={"space-between"}
-                          alignItems={"baseline"}
-                          flexDirection={{ base: "column", md: "row" }}
-                          px={"0px"}
-                          gap={"20px"}
-                      >
-                          <Text fontSize={{ base: "40px", md: "26px" }} ml={'20px'} fontWeight={'700'} maxW={'150px'} color={'#000f41'}>
-                              {item.name}
-                          </Text>
-                          <Flex flexDirection={'column'} justifyContent={'space-between'} h={'70px'}>
-                              <Text fontSize={'24.5px'} fontWeight={'700'}>
-                                  {item.price} &#8381;
-                              </Text>
-                              <Flex >
-                                  <Image src={StarFill} h={'16px'}/>
-                                  <Image src={StarFill} h={'16px'}/>
-                                  <Image src={StarFill} h={'16px'}/>
-                                  <Image src={StarFill} h={'16px'}/>
-                                  <Image src={StarOutline} h={'16px'}/>
-                              </Flex>
-                          </Flex>
-                      </Flex>
-                          <Text fontSize={"16px"} mb={"20px"}>
-                              <Box
-                                  as="span"
-                                  fontSize={"18px"}
-                                  color={"#000f41"}
-                                  fontWeight="bold"
-                              >
-                                  Состав:{" "}
-                              </Box>{" "}
-                              {item.compound}
-                          </Text>
-                          <Text fontSize={"16px"} mb={"20px"}>
-                              <Box
-                                  as="span"
-                                  fontSize={"18px"}
-                                  color={"#000f41"}
-                                  fontWeight="bold"
-                              >
-                                  Цвет:{" "}
-                              </Box>{" "}
-                              {item.color}
-                          </Text>
-                          <Text fontWeight="400" fontSize={"16px"} mb={"20px"}>
-                              <Box
-                                  as="span"
-                                  fontSize={"18px"}
-                                  color={"#000f41"}
-                                  fontWeight="bold"
-                              >
-                                  Описание:{" "}
-                              </Box>{" "}
-                              {formatTextWithLineBreaks(item.description)}
-                          </Text>
-                  </Box>
-              </Flex>
+        <Box>
+            <Container maxW="container.xl">
+                {/*{items.map((item, index) => (*/}
+                <Flex
+                    gap={{base: "20px", md: "40px"}}
+                    alignItems={"start"}
+                    flexDirection={{base: "column", md: "row"}}
+                >
+                    <div className="cards">
+                        <div className="card">
+                            <div className="thumbs">
 
-          ))}
-      </Container>
-    </Box>
-  );
+                                <div className="place" src='../'></div>
+                                <div className="thumb"></div>
+                                <div className="place"></div>
+                                <div className="thumb"></div>
+                                <div className="place"></div>
+                                <div className="thumb"></div>
+
+
+                                {/*    <Image*/}
+                            {/*        className={'place'}*/}
+                            {/*        // src={item.img}*/}
+                            {/*        src={ItemImg}*/}
+                            {/*        w={"auto"}*/}
+                            {/*        h={"600px"}*/}
+                            {/*        objectFit={"contain"}*/}
+                            {/*    />*/}
+                            {/*    <Image className={'thumb'}*/}
+                            {/*        // src={item.img}*/}
+                            {/*        src={ItemImg}*/}
+                            {/*        w={"auto"}*/}
+                            {/*        h={"600px"}*/}
+                            {/*        objectFit={"contain"}*/}
+                            {/*    />*/}
+                            {/*    <Image className={'place'}*/}
+                            {/*        // src={item.img}*/}
+                            {/*        src={ItemImg}*/}
+                            {/*        w={"auto"}*/}
+                            {/*        h={"600px"}*/}
+                            {/*        objectFit={"contain"}*/}
+                            {/*    />*/}
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <Flex flexDirection={'column'}>
+                        <Text fontSize={{base: "40px", md: "26px"}} fontWeight={'700'}
+                              color={'#000f41'}>
+                            {/*{item.name}*/}
+                            Футболка базовая хлопок
+                        </Text>
+                        <HStack spacing={4}>
+                            <HStack spacing={1}>
+                                <Image src={StarFill} h={'16px'}/>
+                                <Image src={StarFill} h={'16px'}/>
+                                <Image src={StarFill} h={'16px'}/>
+                                <Image src={StarFill} h={'16px'}/>
+                                <Image src={StarOutline} h={'16px'}/>
+                            </HStack>
+                            <Text>
+                                100 000+ оценок
+                            </Text>
+                        </HStack>
+                        <HStack>
+                            <Text fontSize={'24.5px'} fontWeight={'700'} color={'#f44'}>
+                                {/*{item.price} &#8381;*/}
+                                1490 &#8381;
+                            </Text>
+                            <Text fontSize={'14px'} fontWeight={'500'} color={'#868695'}
+                                  textDecoration={'line-through'}>
+                                3990 &#8381;
+                            </Text>
+                        </HStack>
+                        <HStack>
+                            <MotionButton
+                                p={"20px"}
+                                borderRadius={"10px"}
+                                fontSize={"16px"}
+                                fontWeight={600}
+                                whileHover={{scale: 1.05}}
+                                h={'36px'} bg={'#a161ff'} _hover={{bg: '#9F30FF'}}
+                                w={'150px'}
+                            >
+                                <Text fontSize={'16px'} color={'#fff'}>WB</Text>
+                            </MotionButton>
+                            <MotionButton
+                                p={"20px"}
+                                borderRadius={"10px"}
+                                fontSize={"16px"}
+                                fontWeight={600}
+                                whileHover={{scale: 1.05}}
+                                h={'36px'} bg={'#005bff'} _hover={{bg: '#286bff'}}
+                                w={'150px'}
+                            >
+                                <Text fontSize={'16px'} color={'#fff'}>OZON</Text>
+                            </MotionButton>
+                        </HStack>
+
+                        <Flex flexDirection={'column'} mt={'40px'}>
+                            <Text fontSize={"16px"} mb={"20px"}>
+                                <Box
+                                    as="span"
+                                    fontSize={"18px"}
+                                    color={"#000f41"}
+                                    fontWeight="bold"
+                                >
+                                    Состав:{" "}
+                                </Box>{" "}
+                                {/*{item.compound}*/}
+                                95% хлопок, 5% лайкра
+                            </Text>
+                            <Text fontSize={"16px"} mb={"20px"}>
+                                <Box
+                                    as="span"
+                                    fontSize={"18px"}
+                                    color={"#000f41"}
+                                    fontWeight="bold"
+                                >
+                                    Цвет:{" "}
+                                </Box>{" "}
+                                {/*{item.color}*/}
+                                Черный
+                            </Text>
+                            <Text fontSize={"16px"} mb={"20px"}>
+                                <Box
+                                    as="span"
+                                    fontSize={"18px"}
+                                    color={"#000f41"}
+                                    fontWeight="bold"
+                                >
+                                    Особенности:{" "}
+                                </Box>{" "}
+                                {/*{item.color}*/}
+                                легкая, тонкая, длинная
+                            </Text>
+                        </Flex>
+                        <Text fontWeight="400" fontSize={"16px"} mb={"20px"}>
+                            <Box
+                                as="span"
+                                fontSize={"18px"}
+                                color={"#000f41"}
+                                fontWeight="bold"
+                            >
+                                Описание:{" "}
+                            </Box>{" "}
+                            {/*{formatTextWithLineBreaks(item.description)}*/}
+                            Невероятно комфортная, приятная к телу базовая футболка правильной длины. Ее удобно
+                            заправлять и носить навыпуск, отлично сочетается с простыми и ультрамодными низами.
+                            Возможность комбинации многослойных образов всегда смотрится выигрышно и подчеркивает
+                            исключительную индивидуальность её обладательницы.
+                        </Text>
+                    </Flex>
+                </Flex>
+
+                {/*))}*/}
+            </Container>
+        </Box>
+    );
 };
 
 export default CatalogPage;
